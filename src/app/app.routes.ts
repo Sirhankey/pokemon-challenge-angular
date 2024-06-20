3088b3856194d43f59825cabc02005c8ec0b465b
Sirhankey <dguimaraes@gmail.com>
2024-06-20 00:57:44 -0300

    altera app component, routes e config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { BoosterShopComponent } from '../pages/booster-shop/booster-shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: BoosterShopComponent }
+
+  // {path: 'decks', component: DecksComponent},
+];
