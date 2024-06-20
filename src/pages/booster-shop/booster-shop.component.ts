d30b2836ed257859317bb644c22d0e57f0cd6774
Sirhankey <dguimaraes@gmail.com>
2024-06-20 01:16:40 -0300

    altera booster shop para acionar modal

--- src/pages/booster-shop/booster-shop.component.ts
+++ src/pages/booster-shop/booster-shop.component.ts
@@ -3,16 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { Card } from '../../models/card.model';
 import { PokemonService } from '../../services/pokemon.service';
 import { getRandomElements } from '../../utils/utility';
+import { CardModalComponent } from '../../components/card-modal/card-modal.component';
 
 @Component({
   selector: 'app-booster-shop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardModalComponent],
   templateUrl: './booster-shop.component.html',
   styleUrl: './booster-shop.component.scss'
 })
 export class BoosterShopComponent implements OnInit {
   cards: Card[] = [];
+  selectedImage: string | null = null;
+  isModalVisible: boolean = false;
 
   constructor(
     private pokemonService: PokemonService
@@ -37,6 +40,16 @@ export class BoosterShopComponent implements OnInit {
 
 
   ngOnInit(): void {
+  }
+
+  showImage(imageSrc: string) {
+    this.selectedImage = imageSrc;
+    this.isModalVisible = true;
+  }
+
+  handleCloseModal() {
+    this.isModalVisible = false;
+    this.selectedImage = null;
   }
 
 }
