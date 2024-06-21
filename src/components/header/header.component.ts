c21e92f0248de88639956b0b9daa0fea93974024
Sirhankey <dguimaraes@gmail.com>
2024-06-20 23:08:33 -0300

    adiciona header

---
+++ src/components/header/header.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Route, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit{
+  trainerName: string = '';
+  money: number = 0;
+  isAuthenticated: boolean = false;
+  user: User | null = null;
+
+  constructor(
+    private authService: AuthService, private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+      this.isAuthenticated = !!user;
+      this.trainerName = user?.name || 'Ash Ketchum';
+      this.money = user?.money || 0;
+    });
+
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
