1c06f903fcf12386a95466c330cbb2548d9a1927
Sirhankey <dguimaraes@gmail.com>
2024-06-20 23:09:06 -0300

    adiciona footer e header ao app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingComponent } from '../components/loading/loading.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, LoadingComponent, CommonModule
+    RouterOutlet, LoadingComponent, HeaderComponent, FooterComponent ,CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
