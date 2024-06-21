e160663a7b7fef708dcd8ef32a53081aaeeb5938
Sirhankey <dguimaraes@gmail.com>
2024-06-21 00:30:21 -0300

    ajusta app e home

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TextTooltipComponent } from '../../components/tooltip/text-tooltip';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TextTooltipComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-
+  tooltipText: string = '';
+  showTooltip: boolean = false;
 
   constructor(
     private router: Router
