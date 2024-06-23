5371cd101c85ed5329f5f5efd88cf6fbc8b12499
Sirhankey <dguimaraes@gmail.com>
2024-06-23 16:44:24 -0300

    adiciona page a route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { BoosterShopComponent } from '../pages/booster-shop/booster-shop.component';
 import { LoginComponent } from '../pages/login/login.component';
-import { AuthGuard } from '../services/auth.guard';
+import { AuthGuard } from '../services/auth/auth.guard';
 import { NotFoundComponent } from '../pages/not-found/not-found.component';
+import { DeckManagerComponent } from '../pages/deck-manager/deck-manager.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'shop', component: BoosterShopComponent, canActivate: [AuthGuard] },
+  { path: 'decks', component: DeckManagerComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' }
