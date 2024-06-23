1fc5a594158ae258c99c2fb60288ee9526038db7
Sirhankey <dguimaraes@gmail.com>
2024-06-23 16:45:07 -0300

    adiciona auth guard e auth service

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { userMocks } from '../../utils/user-mocks';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private userSubject: BehaviorSubject<User | null>;
+  public user$: Observable<User | null>;
+
+  constructor() {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    this.userSubject = new BehaviorSubject<User | null>(user);
+    this.user$ = this.userSubject.asObservable();
+   }
+
+  login(email: string, password: string): boolean {
+
+    if (email === '' || password === '') return false;
+
+    const user = userMocks.find(user => user.email === email && user.password === password);
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      this.userSubject.next(user);
+      return true;
+    }
+
+    return false;
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+  }
+
+  getUser(): User | null{
+    return this.userSubject.value;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.userSubject.value;
+  }
+
+  updateUser(user: any): void {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.userSubject.next(user);
+  }
+}
