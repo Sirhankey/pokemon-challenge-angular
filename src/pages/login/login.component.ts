10f62dac85f726bd7bb4229054b2340157daa426
Sirhankey <dguimaraes@gmail.com>
2024-06-25 01:05:58 -0300

    adiciona validators e forms ao login

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,33 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppComponent } from '../../app/app.component';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AudioService } from '../../services/audio/audio.service';
-import Swal from 'sweetalert2';
 import { TYPE, toast } from '../../utils/toast-utils';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  email: string = '';
-  password: string = '';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
   errorMessage: string = '';
 
   constructor(
-    private authService: AuthService, private router: Router, private appComponent: AppComponent, private audioService: AudioService
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private appComponent: AppComponent,
+    private audioService: AudioService
   ) { }
 
-  login() {
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login(): void {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Preencha todos os campos treinador!';
+      return;
+    }
+
     this.appComponent.showLoading();
+    const { email, password } = this.loginForm.value;
+
     setTimeout(() => {
-      if (this.authService.login(this.email, this.password)) {
+      if (this.authService.login(email, password)) {
         toast(TYPE.SUCCESS, true, 'Seja bem vindo treinador!');
         this.router.navigate(['/home']);
         this.audioService.play();
